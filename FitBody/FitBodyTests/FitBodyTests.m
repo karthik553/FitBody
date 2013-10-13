e7ffbd353d461bea3d6bb197c6f150688ff1e727
ManikantaSanisetty <msanisetty@solstice-consulting.com>
2013-10-12 21:46:13 -0500

    Initial project setup

---
+++ FitBody/FitBodyTests/FitBodyTests.m
@@ -0,0 +1,34 @@
+//
+//  FitBodyTests.m
+//  FitBodyTests
+//
+//  Created by Manikanta.Sanisetty on 10/12/13.
+//  Copyright (c) 2013 SD. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FitBodyTests : XCTestCase
+
+@end
+
+@implementation FitBodyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
