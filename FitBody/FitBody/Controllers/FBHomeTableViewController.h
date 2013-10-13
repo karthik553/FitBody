bf540f0186de3788bb35e82b8bf28d8277354e6c
ManikantaSanisetty <msanisetty@solstice-consulting.com>
2013-10-12 21:51:11 -0500

    Created HomeTableViewController.

---
+++ FitBody/FitBody/Controllers/FBHomeTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FBHomeViewController.h
+//  FitBody
+//
+//  Created by Manikanta.Sanisetty on 10/12/13.
+//  Copyright (c) 2013 SD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FBHomeTableViewController : UITableViewController
+
+@end
